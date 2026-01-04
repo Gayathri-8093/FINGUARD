f5062dd59f9957d0f0572850cf53ce174b705d52
Hemanth Muvvala <Muvvala.Hemanth@cognizant.com>
2026-01-04 10:26:17 +0530

    mention roues for other 4 components

--- src/app/banker/banker.routes.ts
+++ src/app/banker/banker.routes.ts
@@ -3,6 +3,10 @@ import { BankerLayout } from './banker-layout/banker-layout';
 import { BankerDashboard } from './banker-dashboard/banker-dashboard';
 import { CustomerOnboardingList } from '../features/onboarding/customer-onboarding-list/customer-onboarding-list';
 import { NewOnboarding } from '../features/onboarding/new-onboarding/new-onboarding';
+import { TxMonitoring } from '../features/tx-monitoring/tx-monitoring';
+import { RiskScoringAndManagement } from '../features/risk-scoring-and-management/risk-scoring-and-management';
+import { ComplianceAndRegulatory } from '../features/compliance-and-regulatory/compliance-and-regulatory';
+import { AnalyticsAndDashboard } from '../features/analytics-and-dashboard/analytics-and-dashboard';
  
 export const bankerRoutes: Routes = [
   {
@@ -11,6 +15,11 @@ export const bankerRoutes: Routes = [
       {path: 'dashboard', component: BankerDashboard},
       {path: 'onboarding', component: CustomerOnboardingList},
       {path: 'onboarding/new', component: NewOnboarding},
+      { path: 'tx-monitoring', component: TxMonitoring },
+      { path: 'risk', component: RiskScoringAndManagement },
+      { path: 'compliance', component: ComplianceAndRegulatory },
+      { path: 'analytics', component: AnalyticsAndDashboard },
+
       {path: '', redirectTo: 'dashboard', pathMatch: 'full'}
     ]
   }

b682384bc3fe144ca5a48d34e241d5914f0c9659
Gayathri Chintala <Chintala.Gayathri2@cognizant.com>
2026-01-12 14:11:15 +0530

    lazy loading implementation

--- src/app/admin/admin.routes.ts
+++ src/app/admin/admin.routes.ts
@@ -8,12 +8,12 @@ import { KycVerification } from './kyc-verification/kyc-verification';
  
 export const adminRoutes: Routes = [
 
-  { path: 'dashboard', component: AdminDashboard },
+  { path: 'dashboard',loadComponent:()=> import('./admin-dashboard/admin-dashboard').then(m=> m.AdminDashboard) },
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-  {path:'verification',component:KycVerification},
-   { path: 'tx-monitoring', component: TxMonitoring },
-    { path: 'risk', component: RiskScoringAndManagement },
-    { path: 'compliance', component: ComplianceAndRegulatory },
-    { path: 'analytics', component: AnalyticsAndDashboard },
+  {path:'verification', loadComponent:()=> import('./kyc-verification/kyc-verification').then(m=> m.KycVerification)},
+   { path: 'tx-monitoring', loadComponent:()=> import('../features/tx-monitoring/tx-monitoring').then(m=> m.TxMonitoring) },
+  { path: 'risk', loadComponent:()=> import ('../features/risk-scoring-and-management/risk-scoring-and-management').then(m=>m.RiskScoringAndManagement)},
+  { path: 'compliance', loadComponent:()=> import ('../features/compliance-and-regulatory/compliance-and-regulatory').then(m=>m.ComplianceAndRegulatory) },
+  { path: 'analytics', loadComponent:()=> import ('../features/analytics-and-dashboard/analytics-and-dashboard').then(m=>m.AnalyticsAndDashboard)},
 ];
  
